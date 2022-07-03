72eeda3ee6db6558c272828caae3673bcc63e020
jfkeci <ja.lip132@gmail.com>
2022-07-03 10:53:25 +0200

    chore: fix update user dto

--- server/src/resources/auth/dto/login-user.dto.ts
+++ server/src/resources/auth/dto/login-user.dto.ts
@@ -1,13 +1,15 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class LoginUserDto {
   @IsNotEmpty({ groups: ['login-via-username'] })
+  @IsOptional({ groups: ['login-via-email'] })
   @IsString()
   username?: string;
-  @IsNotEmpty({ groups: ['login-via-password'] })
+  @IsNotEmpty({ groups: ['login-via-email'] })
+  @IsOptional({ groups: ['login-via-username'] })
   @IsString()
   email?: string;
-  @IsNotEmpty({ groups: ['login-via-username', 'login-via-password'] })
+  @IsNotEmpty({ groups: ['login-via-username', 'login-via-email'] })
   @IsString()
   password: string;
 }
