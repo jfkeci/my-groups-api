3e31f12235a502175fb0ee678c9a95fe7e25dec7
jfkeci <ja.lip132@gmail.com>
2022-08-04 22:00:46 +0200

    chore: use port from env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(process.env.APP_ROUTE_PREFIX);
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
