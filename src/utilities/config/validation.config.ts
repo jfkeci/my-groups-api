5994e4680d5e2662658c017861f3904c17d17fda
jfkeci <ja.lip132@gmail.com>
2022-08-07 19:09:59 +0200

    chore: alter default port

--- src/utilities/config/validation.config.ts
+++ src/utilities/config/validation.config.ts
@@ -2,9 +2,9 @@ import * as Joi from 'joi';
 
 export default (): Joi.ObjectSchema<any> =>
   Joi.object({
-    PORT: Joi.number().default(13372),
+    PORT: Joi.number().default(3000),
     NODE_ENV: Joi.string().default('development'),
     APP_ROUTE_PREFIX: Joi.string().default('api'),
     DATABASE_URL: Joi.string().exist(),
-    BASE_URL: Joi.string().exist(),
+    BASE_URL: Joi.string().exist()
   });
