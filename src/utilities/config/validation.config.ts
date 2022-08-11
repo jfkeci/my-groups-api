4c9762087c0bdef5f803cacb416ea2d8fce0d438
jfkeci <ja.lip132@gmail.com>
2022-08-11 20:16:21 +0200

    chore: add logger env variables to .env

--- src/utilities/config/validation.config.ts
+++ src/utilities/config/validation.config.ts
@@ -6,5 +6,9 @@ export default (): Joi.ObjectSchema<any> =>
     NODE_ENV: Joi.string().default('development'),
     DATABASE_URL: Joi.string().exist(),
     BASE_URL: Joi.string().exist(),
-    VUE_APP_URL: Joi.string().exist()
+    VUE_APP_URL: Joi.string().exist(),
+    LOG_LEVEL: Joi.string().exist(),
+    LOG_FREQUENCY: Joi.string().exist(),
+    LOG_MAX_FILES: Joi.string().exist(),
+    LOG_MAX_FILE_SIZE: Joi.string().exist()
   });
