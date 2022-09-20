eba3e1d4147caa6b0371cbedc09e44044fc3bcec
jfkeci <ja.lip132@gmail.com>
2022-09-20 06:47:50 +0200

    chore: remove assets module

--- src/utilities/static/assets.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AssetsService } from './service/assets.service';
-import { AssetsController } from './controller/assets.controller';
-
-@Module({
-  controllers: [AssetsController],
-  providers: [AssetsService]
-})
-export class AssetsModule {}
